f3fb33099abc735bc35d5cb9871c4c7ca4990aea
jplaulau14 <prod.patricklaurel@gmail.com>
2023-03-11 09:01:22 +0800

    functions c

---
+++ 02-c-c++-essentials/06-functions/function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x = 10;
+    int y = 20;
+    int z = add(x, y);
+    printf("z: %d", z);
+    return 0;
+}
